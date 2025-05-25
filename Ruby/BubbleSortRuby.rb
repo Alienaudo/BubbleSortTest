14df8e700c2f6666a3f1da607021936dab3c8991
WarRig <warrig@DESKTOP-ACSKR0D.intelbras.local>
2025-05-25 18:05:55 -0400

    Implemtatcao em Ruby

---
+++ Ruby/BubbleSortRuby.rb
@@ -0,0 +1,60 @@
+def bubblesort(array)
+
+    size = array.size - 1
+    swapped = false
+
+    (0...size).each do |i|
+
+        iSize = 0...array.count - 1 - i
+        swapped = false
+
+        (0...(size - i)).each do |j|
+
+            if array[j] > array[j + 1]
+
+
+                array[j], array[j + 1] = array[j + 1], array[j]
+
+                swapped = true
+
+            end
+
+        end
+
+        break unless swapped
+
+    end
+
+    return array
+
+end
+
+numberList = []
+
+File.open("./arr.txt", "r") do |f|
+
+    f.each_line do |line|
+
+        numberList << line.to_i
+
+    end
+
+end
+
+tStart = Time.now
+sortedList = bubblesort(numberList)
+tEnd = Time.now
+
+time = tEnd - tStart
+
+puts "Execution time: #{time.round(2)}"
+
+File.open("./sortedArray.txt", "w") do |f|
+
+    sortedList.each do |number|
+
+        f.write "#{number}\n"
+
+    end
+
+end
